a098bf8c96320f9aeeaf9c0435f5c1c4046f83b8
codeacme17 <vir_kings@qq.com>
2023-11-03 12:51:12 +0800

    chore: add shadc-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from './components/ui/button'
+
 function App() {
   return (
     <>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
