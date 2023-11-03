b64dea3d3fae8de53334464e434fdd7b6d4d494d
codeacme17 <vir_kings@qq.com>
2023-11-03 16:47:40 +0800

    ui: add brand dialog to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,54 @@
 import { useState } from 'react'
 import { useTheme } from '@/contexts/theme-provider'
 import { cn } from '@/lib/utils'
-import { Sun, Moon, AlignLeft, AlignRight } from 'lucide-react'
-import { Button } from './ui/button'
+import { version } from '../../package.json'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Sun, Moon, AlignLeft, AlignRight, Github } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 export const Navbar = () => {
   const { theme, setTheme } = useTheme()
-
   const [isExpanded, setIsExpanded] = useState(false)
 
   return (
     <nav className="fixed top-0 py-4 px-7 w-full">
       <section className="flex justify-between items-center">
-        <div>
-          <img src="/logo.png" alt="" className="w-6" />
-        </div>
+        <Dialog>
+          <DialogTrigger>
+            <img src="/logo.png" alt="Breeze Tab Logo" className="w-6" />
+          </DialogTrigger>
+          <DialogContent className="bg-primary/10">
+            <DialogHeader className="-mt-4">
+              <Button
+                size="icon"
+                variant="ghost"
+                onClick={() =>
+                  window.open('https://github.com/codeacme17/breeze-tab')
+                }>
+                <Github className="w-4 h-4" />
+              </Button>
+            </DialogHeader>
+
+            <div className="flex flex-col items-center justify-center text-primary/60 space-y-0 -mt-6">
+              <div className="mb-3">
+                <img src="/logo.png" alt="Breeze Tab Logo" className="w-24" />
+              </div>
+              <h2 className="text-2xl">Breeze Tab</h2>
+              <p className="text-sm">
+                version
+                <code className="ml-1 px-2 py-0.5 bg-primary/30 rounded-lg">
+                  v{version}
+                </code>
+              </p>
+            </div>
+          </DialogContent>
+        </Dialog>
 
         <div className="ml-auto flex items-center">
           <button
