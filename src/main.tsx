b8e646d59734ec4040f8cf9c2befa47504040821
codeacme17 <vir_kings@qq.com>
2023-11-03 12:38:23 +0800

    refactor: remove unused code and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
-
-root.render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import webExtension, { readJsonFile } from 'vite-plugin-web-extension'
-import path from 'node:path'
+import react from '@vitejs/plugin-react'
 
-function generateManifest() {
-  const manifest = readJsonFile('src/manifest.json')
-  const pkg = readJsonFile('package.json')
-  return {
-    name: 'Breeze Tab',
-    description: pkg.description,
-    version: pkg.version,
-    ...manifest,
-  }
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    webExtension({
-      manifest: generateManifest,
-      additionalInputs: ['src/main.html'],
-    }),
-  ],
-  resolve: {
-    alias: {
-      // In dev mode, make sure fast refresh works
-      '/@react-refresh': path.resolve(
-        'node_modules/@vitejs/plugin-react-swc/refresh-runtime.js'
-      ),
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  plugins: [react()],
 })
