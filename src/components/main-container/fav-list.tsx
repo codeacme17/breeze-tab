d2208b53768a468a4e3949d1b5275f943c9fa96e
leyoonafr <vir_kings@qq.com>
2023-11-06 13:58:45 +0800

    ui: add right click fav item menu

--- src/components/main-container/fav-list.tsx
+++ src/components/main-container/fav-list.tsx
@@ -1,6 +1,67 @@
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu'
+import { Plus, Pencil, Trash2 } from 'lucide-react'
+
+type FavItem = {
+  name: string
+  url: string
+  iconUrl: string
+}
+
 export const FavList = () => {
+  const favList: FavItem[] = [
+    {
+      name: 'Github',
+      url: 'https://github.com',
+      iconUrl:
+        'https://github.githubassets.com/favicons/favicon-dark.png',
+    },
+  ]
+
   return (
     <section className="w-full flex flex-wrap mt-2">
+      {favList.map((item) => (
+        <ContextMenu>
+          <ContextMenuTrigger
+            className="
+              h-32 
+              w-1/5 
+              flex 
+              flex-col 
+              select-none 
+              justify-center 
+              items-center 
+              hover:bg-muted 
+              transition-colors 
+              delay-75 
+              rounded-lg 
+              cursor-pointer
+            ">
+            <div onClick={() => window.location.assign(item.url)}>
+              <div className="w-12 h-12 rounded-full flex justify-center items-center bg-muted-foreground/20">
+                <img src={item.iconUrl} className="w-6 h-6" />
+              </div>
+
+              <p className="text-sm mt-2 text-muted-foreground/70">
+                {item.name}
+              </p>
+            </div>
+          </ContextMenuTrigger>
+          <ContextMenuContent>
+            <ContextMenuItem>
+              <Pencil className="w-4 h-4 mr-2" /> Modify
+            </ContextMenuItem>
+            <ContextMenuItem>
+              <Trash2 className="w-4 h-4 mr-2" /> Remove
+            </ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
+      ))}
+
       <div
         className={`
           h-32 
@@ -16,15 +77,12 @@ export const FavList = () => {
           rounded-lg 
           cursor-pointer
         `}>
-        <div className="w-14 h-14 rounded-full flex justify-center items-center bg-muted-foreground/20">
-          <img
-            src="https://github.githubassets.com/favicons/favicon-dark.png"
-            className="w-6 h-6"
-          />
+        <div className="w-12 h-12 rounded-full flex justify-center items-center bg-muted-foreground/20">
+          <Plus className="w-6 h-6 stroke-muted-foreground" />
         </div>
 
         <p className="text-sm mt-2 text-muted-foreground/70">
-          Github
+          Add New
         </p>
       </div>
     </section>
