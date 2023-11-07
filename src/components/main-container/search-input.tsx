ad0ee3dd910f7332b910c2522c9cfdd4a4a916d1
leyoonafr <vir_kings@qq.com>
2023-11-07 17:29:41 +0800

    feat: add search by fav item search field

--- src/components/main-container/search-input.tsx
+++ src/components/main-container/search-input.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLocalStorage } from 'react-use'
 import { cn } from '@/lib/utils'
 import { SEARCH_ENGINE } from '@/lib/constants'
@@ -11,8 +11,8 @@ type SearchEngine = 'google' | 'bing' | 'baidu' | undefined
 
 export const SearchInput = () => {
   const isExpendFav = useExpendFavStore((state) => state.isExpendFav)
-
   const searchInputRef = useRef<HTMLInputElement>(null)
+
   const [searchEngine, setSearchEngine] =
     useLocalStorage<SearchEngine>('bz:search-engine', 'bing')
   const [searchEngineUrl, setSearchEngineUrl] = useState<string>(
@@ -21,11 +21,48 @@ export const SearchInput = () => {
 
   const favList = useFavListStore((state) => state.favList)
 
+  // Current Fav Item is user enter the shortkey whitch is match the fav item
+  const [currentFavItem, setCurrentFavItem] =
+    useState<FavItem | null>()
+  const [isSeachFieldFocus, setIsSeachFieldFocus] = useState(false)
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const inputValue = e.target.value.trim()
+    setCurrentFavItem(null)
+    setIsSeachFieldFocus(false)
+
+    if (!inputValue) return
+
+    const chunks = inputValue.split(':')
+
+    if (!!chunks[0].trim() && chunks.length > 1) {
+      checkIsSearchField(chunks)
+    } else checkIsShortKey(inputValue)
+  }
+
+  const checkIsShortKey = (inputValue: string) => {
+    const matchItem = favList.find(
+      (item) => item.shortKey === inputValue.toLowerCase()
+    )
+    setCurrentFavItem(matchItem || null)
+  }
+
+  const checkIsSearchField = (chunks: string[]) => {
+    const matchItem = favList.find(
+      (item) => item.shortKey === chunks[0].toLowerCase()
+    )
+    if (!matchItem?.searchField) return
+    setCurrentFavItem(matchItem)
+    setIsSeachFieldFocus(true)
+  }
+
   const handleKeyDown = (
     e: React.KeyboardEvent<HTMLInputElement>
   ) => {
     if (e.key === 'Tab') switchEngine(e)
-    if (e.key === 'Enter') handleEnter(e)
+    if (e.key === 'Enter') handleEnter()
   }
 
   const switchEngine = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -35,22 +72,22 @@ export const SearchInput = () => {
     else if (searchEngine === 'baidu') setSearchEngine('bing')
   }
 
-  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleEnter = () => {
+    // Normal search by search engine
     if (!currentFavItem) return runSearchEngine()
-    window.location.assign(currentFavItem.url)
-  }
-
-  const [currentFavItem, setCurrentFavItem] =
-    useState<FavItem | null>()
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (!e.target.value.trim()) return
-
-    const shortKey = e.target.value.trim().toLowerCase()
-    const target = favList.find((item) => item.shortKey === shortKey)
-    if (target) setCurrentFavItem(target)
-    else setCurrentFavItem(null)
+    // Redirect to the fav item url
+    if (!isSeachFieldFocus)
+      return window.location.assign(currentFavItem.url)
+    // Redirect to the fav item search field url
+    if (isSeachFieldFocus) {
+      const chunks = searchInputRef.current?.value.split(':')
+      const searchUrl =
+        currentFavItem.url +
+        '/' +
+        currentFavItem.searchField +
+        chunks![1].trim()
+      window.location.assign(searchUrl)
+    }
   }
 
   const runSearchEngine = () => {
@@ -99,7 +136,8 @@ export const SearchInput = () => {
         onKeyDown={handleKeyDown}
         onChange={handleInputChange}
         type="text"
-        className={cn(` 
+        className={cn(
+          ` 
           w-full 
           h-12 
           pl-14
@@ -117,7 +155,9 @@ export const SearchInput = () => {
           placeholder:text-muted-foreground/50 
           placeholder:text-base 
           placeholder:select-none 
-          focus:rounded-md`)}
+          focus:rounded-md`,
+          isSeachFieldFocus && 'border-primary'
+        )}
         placeholder="Breezing through the web"
       />
 
