1e13c82fbf5d7d3ee2da11a27d5c814a550585f4
leyoonafr <vir_kings@qq.com>
2023-11-08 11:30:32 +0800

    feat: add `setFavList` to fav store

--- src/store/fav.ts
+++ src/store/fav.ts
@@ -13,6 +13,7 @@ export type FavItem = {
 
 interface FavListState {
   favList: FavItem[]
+  setFavList: (favList: FavItem[]) => void
   addFav: (fav: FavItem) => void
   modifyFav: (fav: FavItem) => void
   removeFav: (fav: FavItem) => void
@@ -20,13 +21,18 @@ interface FavListState {
 
 export const useFavListStore = create<FavListState>((set) => {
   if (!localStorage.getItem('bz:fav-list'))
-    localStorage.setItem(
-      'bz:fav-list',
-      JSON.stringify(DEFAUTL_FAV_LIST)
-    )
+    localStorage.setItem('bz:fav-list', JSON.stringify(DEFAUTL_FAV_LIST))
 
   return {
     favList: JSON.parse(localStorage.getItem('bz:fav-list')!),
+
+    setFavList: (favList: FavItem[]) => {
+      set((state) => {
+        localStorage.setItem('bz:fav-list', JSON.stringify(favList))
+        state.favList = favList
+        return { favList }
+      })
+    },
 
     addFav: (fav: FavItem) => {
       set((state) => {
@@ -50,9 +56,7 @@ export const useFavListStore = create<FavListState>((set) => {
 
     removeFav: (fav: FavItem) => {
       set((state) => {
-        const favList = state.favList.filter(
-          (item) => item.url !== fav.url
-        )
+        const favList = state.favList.filter((item) => item.url !== fav.url)
         localStorage.setItem('bz:fav-list', JSON.stringify(favList))
         return { favList }
       })
@@ -66,8 +70,7 @@ interface ExpendFavState {
 }
 
 export const useExpendFavStore = create<ExpendFavState>((set) => ({
-  isExpendFav:
-    localStorage.getItem('bz:is-fav-expend') === 'true' || false,
+  isExpendFav: localStorage.getItem('bz:is-fav-expend') === 'true' || false,
 
   troggleIsExpendFav: (is: boolean) =>
     set(() => {
