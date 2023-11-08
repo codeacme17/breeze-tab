8b9c23104e7f053b2f6df1758257efbdda4a7e42
leyoonafr <vir_kings@qq.com>
2023-11-08 17:11:35 +0800

    feat: add dnd feature to fav list

--- src/components/main-container/fav-list.tsx
+++ src/components/main-container/fav-list.tsx
@@ -1,6 +1,11 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { FavItem, useFavListStore } from '@/store'
 
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+} from 'react-beautiful-dnd'
 import {
   ContextMenu,
   ContextMenuContent,
@@ -12,6 +17,7 @@ import { FavDialog } from './fav-dialog'
 
 export const FavList = () => {
   const favList = useFavListStore((state) => state.favList)
+  const setFavList = useFavListStore((state) => state.setFavList)
   const removeFav = useFavListStore((state) => state.removeFav)
 
   const [showDialog, setShowDialog] = useState(false)
@@ -31,76 +37,130 @@ export const FavList = () => {
     removeFav(item)
   }
 
+  const onDragEnd = useCallback(
+    (result: any) => {
+      if (!result.destination) return
+
+      const newFavList = reorder(
+        favList,
+        result.source.index,
+        result.destination.index
+      )
+
+      setFavList(newFavList)
+    },
+    [favList, setFavList]
+  )
+
+  const reorder = (
+    list: FavItem[],
+    startIndex: number,
+    endIndex: number
+  ) => {
+    const result = list
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+  }
+
   return (
-    <section className="w-full flex flex-wrap mt-5">
-      {favList!.map((item) => (
-        <ContextMenu key={item!.url}>
-          <ContextMenuTrigger
-            className="
-              h-32 
-              w-1/5 
-              flex 
-              flex-col 
-              select-none 
-              justify-center 
-              items-center 
-              hover:bg-muted 
-              transition-colors 
-              delay-75 
-              rounded-lg 
-              cursor-pointer
-            ">
-            <div onClick={() => window.location.assign(item.url)}>
-              <div className="w-12 mx-auto h-12 rounded-full flex justify-center items-center bg-muted-foreground/20">
-                <img
-                  src={item.logoUrl}
-                  onError={(e: any) =>
-                    (e.target.src = item.canvasLogoUrl)
-                  }
-                  className="w-6 h-6"
-                />
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable
+        droppableId="droppableFavList"
+        direction="horizontal"
+        isCombineEnabled={true}>
+        {(provided, snapshot) => (
+          <section
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="w-full grid grid-cols-5 mt-5">
+            {favList.map((item, index) => (
+              <ContextMenu key={item!.url}>
+                <Draggable
+                  key={item.url}
+                  draggableId={item.url}
+                  index={index}>
+                  {(provided, snapshot) => (
+                    <ContextMenuTrigger
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      className="
+                        h-32 
+                        flex 
+                        flex-col 
+                        select-none 
+                        justify-center 
+                        items-center 
+                        hover:bg-muted 
+                        transition-colors 
+                        delay-75 
+                        rounded-lg 
+                        cursor-pointer
+                      ">
+                      <div
+                        {...provided.dragHandleProps}
+                        onClick={() =>
+                          window.location.assign(item.url)
+                        }>
+                        <div className="w-12 mx-auto h-12 rounded-full flex justify-center items-center bg-muted-foreground/20">
+                          <img
+                            src={item.logoUrl}
+                            onError={(e: any) =>
+                              (e.target.src = item.canvasLogoUrl)
+                            }
+                            className="w-6 h-6"
+                          />
+                        </div>
+
+                        <p className="text-sm truncate w-24 text-center break-words mt-2 text-muted-foreground/70">
+                          {item.label}
+                        </p>
+                      </div>
+
+                      <ContextMenuContent className="bg-background">
+                        <ContextMenuItem
+                          onClick={() => handleModify(item)}>
+                          <Pencil className="w-4 h-4 mr-2" /> Modify
+                        </ContextMenuItem>
+                        <ContextMenuItem
+                          onClick={() => handleRemove(item)}>
+                          <Trash2 className="w-4 h-4 mr-2" /> Remove
+                        </ContextMenuItem>
+                      </ContextMenuContent>
+                    </ContextMenuTrigger>
+                  )}
+                </Draggable>
+              </ContextMenu>
+            ))}
+            {provided.placeholder}
+
+            <div
+              onClick={handleAdd}
+              className="
+                h-32 
+                flex 
+                flex-col 
+                select-none 
+                justify-center 
+                items-center 
+                hover:bg-muted 
+                transition-colors 
+                delay-75 
+                rounded-lg 
+                cursor-pointer
+                ">
+              <div className="w-12 h-12 rounded-full flex justify-center items-center bg-muted-foreground/20">
+                <Plus className="w-6 h-6 stroke-muted-foreground" />
               </div>
 
-              <p className="text-sm truncate w-24 text-center break-words mt-2 text-muted-foreground/70">
-                {item.label}
+              <p className="text-sm mt-2 text-muted-foreground/70">
+                Add New
               </p>
             </div>
-          </ContextMenuTrigger>
-          <ContextMenuContent className="bg-background">
-            <ContextMenuItem onClick={() => handleModify(item)}>
-              <Pencil className="w-4 h-4 mr-2" /> Modify
-            </ContextMenuItem>
-            <ContextMenuItem onClick={() => handleRemove(item)}>
-              <Trash2 className="w-4 h-4 mr-2" /> Remove
-            </ContextMenuItem>
-          </ContextMenuContent>
-        </ContextMenu>
-      ))}
-
-      <div
-        onClick={handleAdd}
-        className={`
-          h-32 
-          w-1/5 
-          flex 
-          flex-col 
-          select-none 
-          justify-center 
-          items-center 
-          hover:bg-muted 
-          transition-colors 
-          delay-75 
-          rounded-lg 
-          cursor-pointer
-        `}>
-        <div className="w-12 h-12 rounded-full flex justify-center items-center bg-muted-foreground/20">
-          <Plus className="w-6 h-6 stroke-muted-foreground" />
-        </div>
-
-        <p className="text-sm mt-2 text-muted-foreground/70">
-          Add New
-        </p>
-      </div>
+          </section>
+        )}
+      </Droppable>
+      {/* Other components remain the same */}
 
       <FavDialog
         type={currentItem ? 'Modify' : 'Add'}
@@ -108,6 +168,6 @@ export const FavList = () => {
         onOpenChange={setShowDialog}
         itemInfo={currentItem}
       />
-    </section>
+    </DragDropContext>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import App from './App.tsx'
 import './global.css'
 
 import { ThemeProvider } from '@/contexts/theme-provider.tsx'
-import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="light">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider defaultTheme="light">
+    <App />
+  </ThemeProvider>
 )
