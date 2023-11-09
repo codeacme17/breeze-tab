6b5f0a3e936ca750e0f3f3213d8a01eb210ed4a9
codeacme17 <vir_kings@qq.com>
2023-11-09 12:04:58 +0800

    feat: building a plug-in framework

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
-
+import webExtension from 'vite-plugin-web-extension'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    webExtension({
+      additionalInputs: ['index.html'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
