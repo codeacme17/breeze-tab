84c8cfff0ebed3ce0ad9261a61d76f2f37d36ab5
codeacme17 <vir_kings@qq.com>
2023-11-11 09:51:39 +0800

    chore: update scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
-import webExtension from 'vite-plugin-web-extension'
+import webExtension, { readJsonFile } from 'vite-plugin-web-extension'
 import { defineConfig } from 'vite'
+
+function generateManifest() {
+  const manifest = readJsonFile('manifest.json')
+  const pkg = readJsonFile('package.json')
+  return {
+    name: 'Breeze Tab',
+    auther: pkg.author,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  }
+}
 
 export default defineConfig({
   plugins: [
     react(),
     webExtension({
+      manifest: generateManifest,
       additionalInputs: ['index.html'],
     }),
   ],
