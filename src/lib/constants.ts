b59d14e9766d0eb6be515d3e2088376fd4ebe703
codeacme17 <vir_kings@qq.com>
2023-11-11 09:23:52 +0800

    chore: update default fav list

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -209,10 +209,10 @@ export const COLORS = {
 export const DEFAUTL_FAV_LIST = [
   {
     id: '1',
-    label: 'Github',
-    url: 'https://github.com',
+    label: 'Breeze Tab',
+    url: 'https://github.com/codeacme17/breeze-tab',
     logoUrl: 'https://github.githubassets.com/favicons/favicon-dark.png',
-    shortKey: 'github',
+    shortKey: '',
     searchField: '',
   },
 ]
