a48e9d5cf5945fad5cfdf6c6cc2dced4114df6f0
leyoonafr <vir_kings@qq.com>
2023-11-22 09:34:19 +0800

    fix: extract rllic css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './global.css'
 import { ThemeProvider } from '@/contexts/theme-provider.tsx'
+
+import './global.css'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme="light">
     <App />
-  </ThemeProvider>,
+  </ThemeProvider>
 )
